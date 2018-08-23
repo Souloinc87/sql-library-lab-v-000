081a608e9734823438311a1ef922106f918e8a91
Marcus  = Soulo <souloinc87@gmail.com>
2018-08-23 09:52:15 +0000

    automatically backed up by learn

--- spec/notes.rb
+++ spec/notes.rb
@@ -1,5 +1,5 @@
 #has 2 series (FAILED - 18)
-  has 6 books (FAILED - 19)
+  #has 6 books (FAILED - 19)
   has 8 characters (FAILED - 20)
   has 2 subgenres (FAILED - 21)
   #has 2 authors (FAILED - 22)

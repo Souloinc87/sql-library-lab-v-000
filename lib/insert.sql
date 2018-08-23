402532944e8b508a9404126c4dd7faa097ad7689
Marcus  = Soulo <souloinc87@gmail.com>
2018-08-23 09:55:51 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,5 @@ INSERT INTO authors (id, name) VALUES (1, "Fyodor Dostoevsky"),
 (2, "William shakespeare")
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Yikes!", 1998, 1),
-
+(2, "Freaked Out", 2009, 2), (3, "Untold Mysteries", 2000, 3), (4, "Tidal Wave", 1996, 4),
+(5, "Nice days", 2010, 5), (6, "The Lost Boys" 1994, 6)
